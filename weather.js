5467564467884d39f38441639abf7bcce75f53f5
fatihyalcn <fatihyalcn9332@gmail.com>
2022-03-14 19:19:07 +0100

    update

---
+++ weather.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("section.top-banner form");
+const input = document.querySelector("div.container input");
+const msg = document.querySelector("span.msg")
+const list = document.querySelector("ajax-section .cities")
